f4eb4a9108563da31fd3c46e3e1ee7a833d27f27
uday183 <35099166+uday183@users.noreply.github.com>
2018-01-04 16:23:20 +0530

    Update Djangosignals.py

--- Djangosignals.py
+++ Djangosignals.py
@@ -1,21 +1,19 @@
-Django signals:
+#Django signals:
 
-1 Step: 
+#1 Step: 
 
-create default config app file name in __init__.py file
-Ex:-default_app_config = 'yourapplicationname.apps.yourappconfigfilename'
+#create default config app file name in __init__.py file
+default_app_config = 'yourapplicationname.apps.yourappconfigfilename'
 
-2 Step: 
+#2 Step: 
 
-write a function in apps.py and import your config file in that function
-Ex:-def ready(self):
-        import yourapplicationname.signals
+#write a function in apps.py and import your config file in that function
+def ready(self):
+    import yourapplicationname.signals
 
-3 Step:
-write you signal.py file in your application
-
-Ex:-
-from django.db.models.signals import post_save, pre_save
+#3 Step:
+#write you signal.py file in your application
+from django.db.models.signals import post_save
 
 def yourfunctioname(**kwargs):
     
