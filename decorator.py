485e6cf80bef26b444265a6b18e1cb049dfc5dbc
uday <udaykumar.p@mindlogicx.com>
2019-03-05 15:17:38 +0530

    sample decorator

---
+++ decorator.py
@@ -0,0 +1,29 @@
+
+#function inside function
+def f():
+    def g():
+        print ("this is inside function 'g' ")
+    print ("this is out side function 'f' ")
+    g()
+
+f()
+
+#function as a parameter
+def g():
+    print ("function 'g'")
+def f(func):
+    print ("function 'f'")
+    func()
+f(g)
+
+def deco(f):
+    def inner_deco(v1,v2):
+        print ('inner deco:',v1-v2)
+    return inner_deco
+
+@deco
+def test_deco(v1,v2):
+    print ('test_deco:',v1+v2)
+
+
+test_deco(2,3)
